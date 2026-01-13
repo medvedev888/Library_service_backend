26d71dc772a6c1246ec498c12acba802272436ec
vladislavmedvedev <EnoT3581@mail.ru>
2026-01-13 19:06:51 +0300

    Initial commit

---
+++ src/main/java/me/vladislav/library_service_backend/LibraryServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package me.vladislav.library_service_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryServiceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/vladislav/library_service_backend/LibraryServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vladislav.library_service_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
