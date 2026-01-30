d0ed8b45b08ee3c16066dbb29c7e5527a1b78259
vladislavmedvedev <EnoT3581@mail.ru>
2026-01-30 14:39:11 +0300

    feat(book): introduce BookCopy entity
    Added BookCopy to track number and status of book copies per library.
    Ensures unique book-library pair with database constraint.

---
+++ src/main/java/me/vladislav/library_service_backend/book/model/BookCopy.java
@@ -0,0 +1,44 @@
+package me.vladislav.library_service_backend.book.model;
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(
+        name = "book_copies",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_book_library",
+                        columnNames = {"book_id", "library_id"}
+                )
+        }
+)
+public class BookCopy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "library_id", nullable = false)
+    private Library library;
+
+    @Column(nullable = false)
+    private Long totalCopies;
+
+    @Column(nullable = false)
+    private Long availableCopies;
+}
+
