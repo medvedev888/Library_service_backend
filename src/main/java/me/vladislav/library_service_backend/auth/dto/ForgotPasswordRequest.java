8ff5a881407ad63d08c6dfb7a333be23ccff21c6
misticslippers <eternal.martyr13579@gmail.com>
2026-02-16 18:20:15 +0300

    add ForgotPasswordRequest

---
+++ src/main/java/me/vladislav/library_service_backend/auth/dto/ForgotPasswordRequest.java
@@ -0,0 +1,12 @@
+package me.vladislav.library_service_backend.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ForgotPasswordRequest {
+    @NotBlank
+    @Email
+    private String email;
+}
