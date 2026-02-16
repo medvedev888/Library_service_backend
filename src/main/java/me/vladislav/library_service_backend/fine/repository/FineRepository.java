31de6a3b5122cd46c918be3b0e050fdb3f871009
misticslippers <eternal.martyr13579@gmail.com>
2026-02-16 10:36:30 +0300

    update FineRepository

--- src/main/java/me/vladislav/library_service_backend/fine/repository/FineRepository.java
+++ src/main/java/me/vladislav/library_service_backend/fine/repository/FineRepository.java
@@ -3,11 +3,18 @@ package me.vladislav.library_service_backend.fine.repository;
 import me.vladislav.library_service_backend.fine.model.Fine;
 import me.vladislav.library_service_backend.loan.model.BookLoan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FineRepository extends JpaRepository<Fine, Long> {
+
     Optional<Fine> findByBookLoan(BookLoan bookLoan);
+
+    @Query("select f from Fine f where f.bookLoan.user.id = :userId")
+    List<Fine> findAllByUserId(@Param("userId") Long userId);
 }
