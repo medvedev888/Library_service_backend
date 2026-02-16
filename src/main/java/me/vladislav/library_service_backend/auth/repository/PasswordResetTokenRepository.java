626da11f814b0700b7ba6269d0f146f629f398e3
misticslippers <eternal.martyr13579@gmail.com>
2026-02-16 18:22:21 +0300

    add PasswordResetTokenRepository

---
+++ src/main/java/me/vladislav/library_service_backend/auth/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,12 @@
+package me.vladislav.library_service_backend.auth.repository;
+
+import me.vladislav.library_service_backend.auth.model.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
+}
