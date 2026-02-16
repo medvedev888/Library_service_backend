a24f3ab1185357ca4bda479b5ac453c946b1e412
misticslippers <eternal.martyr13579@gmail.com>
2026-02-16 18:21:34 +0300

    add MailService

---
+++ src/main/java/me/vladislav/library_service_backend/common/mail/MailService.java
@@ -0,0 +1,25 @@
+package me.vladislav.library_service_backend.common.mail;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MailService {
+    private final JavaMailSender mailSender;
+
+    public void sendPasswordResetEmail(String to, String resetLink) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(to);
+        msg.setSubject("Восстановление пароля");
+        msg.setText(
+                "Вы запросили восстановление пароля.\n\n" +
+                        "Ссылка для сброса (действует ограниченное время):\n" +
+                        resetLink + "\n\n" +
+                        "Если это были не вы — просто игнорируйте письмо."
+        );
+        mailSender.send(msg);
+    }
+}
